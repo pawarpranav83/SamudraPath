f67532b95c00186e493196d8bda5b2f05eaf28d1
Gaurang <gssondur28@gmail.com>
2024-12-09 21:11:01 +0530

    added context api

--- samudrapath/src/App.js
+++ samudrapath/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  
-import ShipHealth from "./Pages/ShipHealth";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import './style.css'; // Import the global CSS file
+import ShipHealth from "./Pages/ShipHealth"
+import { ShipProvider } from "./ShipContext";
 
 const App = () => {
   return (
-    <div className="h-screen ">
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/> }/>
-        <Route path="/shiphealth" element={<ShipHealth />} />
-      </Routes>
-    </Router>
+    <div className="h-screen">
+      {/* Wrap the context provider around the Router */}
+      <ShipProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/shiphealth" element={<ShipHealth />} />
+          </Routes>
+        </Router>
+      </ShipProvider>
     </div>
   );
 };
